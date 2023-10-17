5be2428404547cab1c41d194b498af5c05cec58d
Ashraful3605 <ashraful534islam@gmail.com>
2023-10-17 14:08:53 +0100

    Updated Camera Controller
    According to what I have learnt in today's workshop sheet surrounding 3rd Person Camera's I attempted to copy a similar format of 3rd Person Camera while maintaining it as my own project.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,24 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class ThirdPersonCamera : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float sensitivity = 5.0f;
-    
+    public Transform target;
+    public float distance = 5.0f;
+    public float sensitivity = 2.0f;
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        //transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        HandleCameraInput();
+    }
 
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
+    void LateUpdate()
+    {
+        FollowTarget();
+    }
+
+    void HandleCameraInput()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        rotationX -= mouseY;
+        rotationY -= mouseX;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        target.Rotate(Vector3.up * mouseX);
+    }
+    void FollowTarget()
+    {
+        Vector3 targetPosition = target.position - transform.forward * distance;
+        transform.position = targetPosition;
     }
 }
