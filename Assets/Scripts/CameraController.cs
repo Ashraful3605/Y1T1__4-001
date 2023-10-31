9f6b395cb97586f128c6df869d07544478248bb8
Ashraful3605 <ashraful534islam@gmail.com>
2023-10-31 16:15:03 +0000

    Added comments to code.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour
     public float sensitivity = 2.0f;
     private float rotationX = 0.0f;
     private float rotationY = 0.0f;
-    public Vector3 playerDirection;
+    public Vector3 playerPosition;
 
 
     // Start is called before the first frame update
@@ -23,7 +23,7 @@ public class CameraController : MonoBehaviour
 
     void awake()
     {
-        playerDirection = transform.localPosition.normalized;
+        playerPosition = transform.localPosition.normalized; 
     }
 
     // Update is called once per frame
@@ -31,26 +31,26 @@ public class CameraController : MonoBehaviour
     {
         HandleCameraInput();
         
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //sprint bind to move camera back
         {
             distance = distance + 0.5f;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift)) // reset camera back to normal
         {
             distance = 3.0f;
         }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl)) //crouch bind to move camera forward
         {
             distance = distance - 0.5f;
         }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
+        if (Input.GetKeyUp(KeyCode.LeftControl)) // reset camera back to normal
         {
             distance = 3.0f;
         }
 
-        LayerMask wall = LayerMask.GetMask("Wall");
+        LayerMask wall = LayerMask.GetMask("Wall"); // check the layer that it is a wall
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), 2.8f, wall))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), 2.8f, wall)) // fire a raycast from the camera to the player and if there is something between that distance then...
         {
             //Debug.Log("Hit wall");
             //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.green, wall);
@@ -58,30 +58,30 @@ public class CameraController : MonoBehaviour
     }
     void LateUpdate()
     {
-        FollowTarget();
+        FollowTarget(); // have the camera in its fixed position from the camera after the input has been dealt with and the distance from the player has been decided based on the input
     }
 
-    void HandleCameraInput()
+    void HandleCameraInput() //get input from mouse and translate it to the movement of the camera
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
-        rotationX -= mouseY;
-        rotationY += mouseX;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity; //get x input from mouse
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity; //get y input from mouse
+        rotationX -= mouseY; //inverts or reverts the movement in relation to the game
+        rotationY += mouseX; //inverts or reverts the movement in relation to the game 
         if (distance > 2.0f)
         {
-            rotationX = Mathf.Clamp(rotationX, 0, 8);
+            rotationX = Mathf.Clamp(rotationX, 0, 8); //restricts the movement of the camera so the player cannot look through the ceiling
         }
         else
         {
-            rotationX = Mathf.Clamp(rotationX, 0, 14);
+            rotationX = Mathf.Clamp(rotationX, 0, 14); //restricts the movement of the camera so the player cannot look through the floor
         }
-        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
-        target.Rotate(Vector3.up * -mouseX);
+        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0); 
+        target.Rotate(Vector3.up * -mouseX); //rotates the player object in relation to the movement
     }
-    void FollowTarget()
+    void FollowTarget() // allows for the 3rd person aspect
     {
-        targetPosition = target.position - transform.forward * distance;
-        transform.position = targetPosition;
+        targetPosition = target.position - transform.forward * distance; // the desired location of the camera according to the input or movement as a result
+        transform.position = targetPosition; // moves the camera back that much
     }
     
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,28 +12,28 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position;
-        rb = GetComponent<Rigidbody>();
+        startPos = transform.position; //sets the starting position of the character for use later
+        rb = GetComponent<Rigidbody>(); //sets a rigidbody to the rb variable
      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftControl)) //the actual speed of the movement when sprinting is decided 
         {
-            currentSpeed = baseSpeed - 1f;
+            currentSpeed = baseSpeed - 2f;
         }
-        else
+        else // if neither pressed, then there should be no change but if there is a change then it will set it back to the original value
         {
             currentSpeed = baseSpeed;
         }
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift)) //the actual speed of the movement when crouching is decided
         {
             currentSpeed = sprint;
         }
-
+        //whole section deals with moving the player in accordance to the camera, where the front is decided by the opposite position of the camera in terms of x and moving forward according to the input and which was is forward.
         Transform camTransform = Camera.main.transform;
         Vector3 camPosition = new Vector3(camTransform.position.x, transform.position.y, camTransform.position.z);
         Vector3 direction = (transform.position - camPosition).normalized;
